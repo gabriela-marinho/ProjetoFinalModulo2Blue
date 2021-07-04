8889a047fb9e92a5aab026f081b9fd1c42a7e40c
gabriela-marinho <g.barreto.marinho@gmail.com>
2021-07-04 00:30:26 -0300

    link com o bd feito

--- app.py
+++ app.py
@@ -1,42 +1,59 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Configurações de acesso ao banco de dados
+user = 'uutnfujn'
+password = 'TJ-budc9lkNSwDgt29vxcSjalpXx2bj7'
+host = 'tuffi.db.elephantsql.com'
+database = 'uutnfujn'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{database}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "chave escondidamente"
+
+# Instanciando objeto da classe SQLAlchemy
+db = SQLAlchemy(app)
+
+# Modelar a Classe Filmes -> tabela filmes
+class racas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(255), nullable=False)
+    imagem = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, nome, imageml):
+        self.nome = nome
+        self.imagem_url = imagem
+    
+    @staticmethod
+    def read_all():
+        # SELECT * FROM filmes ORDER BY id ASC
+        return racas.query.order_by(racas.id.asc()).all()
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-registros = [
-    {
-        "id": 1,
-        "nome": "Labrador",
-        "imagem_url":"https://www.pngkey.com/png/detail/81-818369_labrador-retriever-transparent-image-labrador-retriever.png"
-    },
-    {
-        "id": 2,
-        "nome": "SRD",
-        "imagem_url":"https://blackwhitepet.com.br/resources/img/racas-de-cachorro-vira-lata-srd.jpg"
-    },
-    {
-        "id": 3,
-        "nome": "Pastor Belga",
-        "imagem_url":"https://www.racasdecachorro.com.br/wp-content/uploads/2018/11/sh_groenendael_495986761.jpg"
-    },
-]
 
-#realizado alteraçoes de novo
 @app.route("/read")
 def read_all():
+    registros = racas.read_all()
+    # Chamada do método read_all da classe Filmes, que representa a tabela filmes do banco de dados.
+    registros = racas.read_all()
     return render_template("read_all.html", registros=registros)
 
-@app.route("/read/<id_registro>") #verificar pq esta dando erro
+
+@app.route("/read/<id_registro>")
 def read_id(id_registro):
-    return render_template("curiosidades_id_1.html")
+    return "Em construção - Visualizar registro de ID "+id_registro
 
 
 @app.route("/create")
 def create():
-    return "Calma Maxu!"
+    return "Em construção - Ainda será feito o CREATE!"
+
 
 if (__name__ == "__main__"):
     app.run(debug=True)
